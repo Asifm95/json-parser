fd34ba7d7f8a52ff3bd594ea8d47695f5ff3e54a
amalshehu <amalshehu@gmail.com>
2018-03-06 17:05:53 +0530

    Fix boolean parser

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 // var json = '{"player": "Dexter", "score": 500}';
 var json = '123 is not null, and null is good.';
+var nullJson = 'null 123';
+var booleanJson = 'true hello';
 function parseJson(json) {
-  console.log(nullParser(json));
+  console.log('Null parser', nullParser(json));
+  console.log('Boolean parser', booleanParser(json));
 }
 function nullParser(str) {
-  const re = /null/gi;
-  let match = str.match(re).map(x => null);
-  let remaining = str.replace(/null/gi, '');
-  return [match, remaining];
+  const re = /^null/gi;
+  let match = str.match(re);
+  return match ? [null, str.replace(/^null/, '')] : null;
 }
-parseJson(json);
+function booleanParser(str) {
+  const re = /^true|false/;
+  let match = str.match(re);
+  return match
+    ? [match[0] ? true : false, str.replace(/^true|false/, '')]
+    : false;
+}
+parseJson(nullJson);
+parseJson(booleanJson);
