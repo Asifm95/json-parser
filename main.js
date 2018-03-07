e9233b151f1f77145005b1f03e54e36930f5324e
amalshehu <amalshehu@gmail.com>
2018-03-07 14:49:46 +0530

    Fix string parser

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // var json = '{"player": "Dexter", "score": 500}';
-let json = '123 is not null, and null is good.';
+let json = 'Good morning th\\nis is a test %^&(*%^ string 111222.';
 let nullJson = 'null 123';
 let numberJson = 'true 30e4';
 let booleanJson = 'true hello';
@@ -7,23 +7,29 @@ function parseJson(json) {
   console.log('Null parser', nullParser(json));
   console.log('Boolean parser', booleanParser(json));
 }
-function nullParser(str) {
+const nullParser = str => {
   const re = /^null/gi;
   let match = str.match(re);
   return match ? [null, str.replace(/^null/, '')] : null;
-}
-function booleanParser(str) {
+};
+const booleanParser = str => {
   const re = /^true|false/;
   let match = str.match(re);
   return match ? [match[0] ? true : false, str.replace(re, '')] : false;
-}
-function numberParser(str) {
+};
+const numberParser = str => {
   const re = /[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/;
   let match = str.match(re);
   return match[0] ? [parseFloat(match[0]), str.replace(re, '')] : null;
-}
-function stringParser(str) {}
+};
+const stringParser = str => {
+  str = str.toString();
+  const re = /[\n|"']/;
+  let slashTest = re.test(str);
+  return slashTest ? ['Error'] : str;
+};
 
 // parseJson(nullJson);
 // parseJson(booleanJson);
-console.log(numberParser(numberJson));
+// console.log(numberParser(numberJson));
+console.log(stringParser(json));
