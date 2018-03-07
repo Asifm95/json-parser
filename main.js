747ee380e5a9ca3c6bd397bfbbf61535df0feaea
amalshehu <amalshehu@gmail.com>
2018-03-08 00:41:56 +0530

    Code cleanup

--- main.js
+++ main.js
@@ -1,63 +1,13 @@
-var fs = require('fs');
-let contents = fs.readFileSync('input.txt', 'utf8');
+import { contents } from './src/fileReader.js';
+import * as parsers from './src/parsers.js';
 
-// function parseJson(json) {
-//   console.log('Null parser', nullParser(json));
-//   console.log('Boolean parser', booleanParser(json));
-// }
-const nullParser = str => {
-  const re = /^null/gi;
-  let match = str.match(re);
-  return match ? [null, str.replace(/^null/, '')] : null;
+const parseJson = json => {
+  // Main parser definition
 };
-const booleanParser = str => {
-  const re = /^true|false/;
-  let match = str.match(re);
-  return match ? [match[0] ? true : false, str.replace(re, '')] : false;
-};
-const numberParser = str => {
-  const re = /[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/;
-  let match = str.match(re);
-  return match[0] ? [parseFloat(match[0]), str.replace(re, '')] : null;
-};
-const stringParser = str => {
-  str = str.toString();
-  const re = /[\n|"']/;
-  let slashTest = re.test(str);
-  return slashTest ? ['Error'] : str;
-};
-const commaParser = str => {
-  const re = /,/;
-  let match = str.match(re);
-  return match ? [match[0], str.replace(re, '')] : null;
-};
-const spaceParser = str => {
-  const re = /^\s/;
-  let match = str.match(re);
-  return match ? [match[0], str.replace(re, '')] : null;
-};
-const colonParser = str => {
-  const re = /^:/;
-  let match = str.match(re);
-  return match ? [match[0], str.replace(re, '')] : null;
-};
-const arrayParser = () => {};
-const objectParser = () => {};
-parsers = [
-  nullParser,
-  booleanParser,
-  numberParser,
-  stringParser,
-  arrayParser,
-  objectParser,
-  commaParser,
-  colonParser,
-  spaceParser
-];
-let valueParser = function factoryParser(...parsers) {
+const factoryParser = function(parsers) {
   for (let i = 0; i < parsers.length; i++) {
-    parsers[i];
+    // parsers[i]();
   }
 };
-// console.log(nullParser(contents));
-console.log(valueParser('null'));
+const valueParser = factoryParser(parsers);
+console.log(parsers);
