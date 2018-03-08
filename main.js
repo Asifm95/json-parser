d0f5512412230a2afdf486c640500447249993c8
amalshehu <amalshehu@gmail.com>
2018-03-08 11:29:48 +0530

    Fix all null returns

--- main.js
+++ main.js
@@ -13,37 +13,41 @@ const nullParser = str => {
 const booleanParser = str => {
   const re = /^true|false/;
   let match = str.match(re);
-  return match ? [match[0] ? true : false, str.replace(re, '')] : false;
+  return match ? [match[0] ? true : false, str.replace(re, '')] : null;
 };
 const numberParser = str => {
   const re = /[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/;
   let match = str.match(re);
-  return match[0] ? [parseFloat(match[0]), str.replace(re, '')] : null;
+  return match ? [parseFloat(match[0]), str.replace(re, '')] : null;
 };
 const stringParser = str => {
   str = str.toString();
   const re = /[\n|"']/;
   let slashTest = re.test(str);
-  return slashTest ? ['Error'] : str;
+  return slashTest ? ['Error'] : [str];
 };
 const commaParser = str => {
   const re = /,/;
   let match = str.match(re);
-  return match ? [match[0], str.replace(re, '')] : null;
+  return match ? [match, str.replace(re, '')] : null;
 };
 const spaceParser = str => {
   const re = /^\s/;
   let match = str.match(re);
-  return match ? [match[0], str.replace(re, '')] : null;
+  return match ? [match, str.replace(re, '')] : null;
 };
 const colonParser = str => {
   const re = /^:/;
   let match = str.match(re);
-  return match ? [match[0], str.replace(re, '')] : null;
+  return match ? [match, str.replace(re, '')] : null;
 };
-const arrayParser = () => {};
-const objectParser = () => {};
-parsers = [
+const arrayParser = () => {
+  return null;
+};
+const objectParser = () => {
+  return null;
+};
+const parsers = [
   nullParser,
   booleanParser,
   numberParser,
@@ -54,10 +58,14 @@ parsers = [
   colonParser,
   spaceParser
 ];
-let valueParser = function factoryParser(...parsers) {
-  for (let i = 0; i < parsers.length; i++) {
-    parsers[i];
-  }
-};
+
+function factoryParser(p) {
+  return function(text) {
+    for (let i = 0; i < p.length; i++) {
+      console.log(p[i](text));
+    }
+  };
+}
+let valueParser = factoryParser(parsers);
 // console.log(nullParser(contents));
-console.log(valueParser('null'));
+valueParser('nullabc123');
