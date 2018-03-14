a421c40da069cce7408d6c9dce5ad574be05a52c
amalshehu <amalshehu@gmail.com>
2018-03-14 21:13:33 +0530

    Modify factory

--- src/modules/factoryParser.js
+++ src/modules/factoryParser.js
@@ -1,10 +1,11 @@
 export const factoryParser = p => {
-  return function(text) {
+  return text => {
     if (text === null) return null
     let out
-    for (let i = 0; i < p.length; i++) {
+    const keys = Object.keys(p)
+    for (let i = 0; i < keys.length; i++) {
       try {
-        out = p[i](text)
+        out = p[keys[i]](text)
       } catch (error) {
         console.log(error)
       }
