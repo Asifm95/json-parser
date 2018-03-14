5a6811618cb209bf997c43528f90327128b02c71
amalshehu <amalshehu@gmail.com>
2018-03-14 23:56:32 +0530

    Code cleanup

--- src/modules/parsers.js
+++ src/modules/parsers.js
@@ -1,94 +1,17 @@
-import { factoryParser } from './factoryParser.js'
-import { colonParser, commaParser, spaceParser } from './subParsers.js'
-import { arrayParser } from './arrayParser.js'
-
-const nullParser = str => {
-  return str && str.startsWith('null') ? [null, str.slice(4)] : null
-}
-
-const booleanParser = str => {
-  return str != undefined && str.startsWith('false')
-    ? [false, str.slice(5)]
-    : str && str.startsWith('true') ? [true, str.slice(4)] : null
-}
-
-const numberParser = str => {
-  let match
-  spaceParser(str) ? (str = spaceParser(str)[1]) : str
-  return (
-    (match = str.match(/^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/)),
-    match ? [parseFloat(match[0]), str.slice(match[0].length)] : null
-  )
-}
-
-const stringParser = str => {
-  let match
-  return str.startsWith('"')
-    ? ((match = str.match(/^"(?:\\"|.)*?"/)),
-      match[0] != undefined
-        ? [stringEnhancer(match[0].slice(1, -1)), str.replace(match[0], '')]
-        : null)
-    : null
-}
-const stringEnhancer = str => {
-  for (const key in regexTable) {
-    str = str.replace(new RegExp(key, 'g'), regexTable[key])
-  }
-  return str
-}
-const regexTable = {
-  '/\\\\/': '\\',
-  '/\\//': '/',
-  '/\\b/': '\b',
-  '/\\f/': '\f',
-  '/\\n/': '\n',
-  '/\\r/': '\r',
-  '/\\t/': '\t'
-}
-
-const objectParser = str => {
-  if (str[0] !== '{') return null
-  // if (str.match(/\,(?!\s*[\{\"\w])/g)) {
-  //   throw SyntaxError('Invalid JSON')
-  // }
-  let object = {}
-  str = str.slice(1)
-  if (str.startsWith(' ') || str.startsWith('\n') || str.startsWith('\t')) {
-    spaceParser(str) ? (str = spaceParser(str)[1]) : str
-  }
-  while (str[0] != '}') {
-    str.startsWith(' ') && spaceParser(str) ? (str = spaceParser(str)[1]) : str
-    let factoryOutput
-    factoryOutput = stringParser(str)
-
-    if (factoryOutput) {
-      let key = factoryOutput[0]
-      if (factoryOutput[1]) {
-        str = factoryOutput[1]
-        str.startsWith(' ') && spaceParser(str)
-          ? (str = spaceParser(str)[1])
-          : str
-        colonParser(str) ? (str = colonParser(str)[1]) : str
-        str.startsWith(' ') && spaceParser(str)
-          ? (str = spaceParser(str)[1])
-          : str
-        let value = valueParser(str)
-        object[key] = value[0]
-        str = value[1]
-        commaParser(str) ? (str = commaParser(str)[1]) : str
-      }
-    }
-    if (str === '}') break
-  }
-  return [object, str.slice(1)]
-}
+import { factory } from './factory/factory.js'
+import { nullx } from './parsers/null.js'
+import { boolean } from './parsers/boolean.js'
+import { number } from './parsers/number.js'
+import { stringx } from './parsers/string.js'
+import { array } from './parsers/array.js'
+import { object } from './parsers/object.js'
 
 export const parsers = {
-  nullParser,
-  booleanParser,
-  numberParser,
-  stringParser,
-  arrayParser,
-  objectParser
+  nullx,
+  boolean,
+  number,
+  stringx,
+  array,
+  object
 }
-export const valueParser = factoryParser(parsers)
+export const valueParser = factory(parsers)
