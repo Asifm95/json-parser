b1d1c55d86abedaeb3cb96f712be96e4657702d6
amalshehu <amalshehu@gmail.com>
2018-03-14 23:04:54 +0530

    Add stringify method to display object on console.

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import { data } from './src/modules/fileReader.js'
-import { parsers, valueParser } from './src/modules/parsers.js'
+import { valueParser } from './src/modules/parsers.js'
 
-console.log(valueParser(data))
+console.time()
+const result = valueParser(data)
+console.timeEnd()
+console.log(JSON.stringify(result[0], true, 10))
