bbf7ec8f40b585f75ade715644362fc7118a9bb8
amalshehu <amalshehu@gmail.com>
2018-03-15 10:16:05 +0530

    Add error handler

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 import { data } from './src/modules/fileReader.js'
 import { valueParser } from './src/modules/parsers.js'
+import { validateRe } from './src/modules/regex/rgx.js'
 
 console.time()
 const result = valueParser(data)
 console.timeEnd()
-console.log(JSON.stringify(result[0], true, 10))
+if (result) {
+  const test = validateRe.test(result[1])
+  test
+    ? console.log('Invalid JSON')
+    : console.log(JSON.stringify(result[0], true, 10))
+}
--- src/modules/regex/rgx.js
+++ src/modules/regex/rgx.js
@@ -14,3 +14,4 @@ export const regexTable = {
   '/\\t/': '\t'
 }
 export const serializeRe = /\s(?=("[^"]*"|[^"])*$)/gm
+export const validateRe = /[^ ]+$/
