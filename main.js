6b3e89571ec3114c5c7c57f6584c58c7a35472a7
amalshehu <amalshehu@gmail.com>
2018-03-18 23:13:02 +0530

    Reformat code

--- main.js
+++ main.js
@@ -1,148 +1,14 @@
 let fs = require('fs')
 let contents = fs.readFileSync('input.json', 'utf8')
 
-const parseJson = json => {
-  const parsed = valueParser(json)
-  if (parsed) {
-    const test = /[^ ]+$/.test(parsed[1])
-    test
-      ? console.log('Invalid JSON')
-      : console.log(JSON.stringify(parsed[0], true, 10))
-  }
-}
-
-const nullParser = str => {
-  return str && str.startsWith('null') ? [null, str.slice(4)] : null
-}
-
-const booleanParser = str => {
-  return str != undefined && str.startsWith('false')
-    ? [false, str.slice(5)]
-    : str && str.startsWith('true') ? [true, str.slice(4)] : null
-}
-
-const numberParser = str => {
-  spaceParser(str) ? (str = spaceParser(str)[1]) : str
-  return (
-    (match = str.match(/^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/)),
-    match ? [parseFloat(match[0]), str.slice(match[0].length)] : null
-  )
-}
-
-const stringParser = str => {
-  return str.startsWith('"')
-    ? ((match = str.match(/^"(?:\\"|.)*?"/)),
-      match[0] != undefined
-        ? [stringEnhancer(match[0].slice(1, -1)), str.replace(match[0], '')]
-        : null)
-    : null
-}
-const stringEnhancer = str => {
-  for (const key in regexTable) {
-    str = str.replace(new RegExp(key, 'g'), regexTable[key])
-  }
-  return str
-}
-const regexTable = {
-  '/\\\\/': '\\',
-  '/\\//': '/',
-  '/\\b/': '\b',
-  '/\\f/': '\f',
-  '/\\n/': '\n',
-  '/\\r/': '\r',
-  '/\\t/': '\t'
-}
-
-const commaParser = str => {
-  return str && str.startsWith(',')
-    ? ((match = str.match(/^,/)), match ? [match[0], str.slice(1)] : null)
-    : null
-}
-
-const spaceParser = str => {
-  return (str && str.startsWith(' ')) || (str && str.startsWith('\n'))
-    ? ((match = str.match(/^\s+|\s+$/)),
-      match && match[0] ? [match[0], str.replace(/^\s+|\s+$/, '')] : null)
-    : null
-}
-const colonParser = str => {
-  return (match = str.match(/^:/)), match ? [match, str.slice(1)] : null
-}
-
-const arrayParser = str => {
-  if (str != undefined && !str.startsWith('[')) {
-    return null
-  }
-  let array = []
-  str = str.slice(1)
-  while (str[0] !== ']') {
-    spaceParser(str) ? (str = spaceParser(str)[1]) : str
-    const factoryOutput = valueParser(str)
-    factoryOutput != null
-      ? (array.push(factoryOutput[0]),
-        (str = factoryOutput[1]),
-        commaParser(factoryOutput[1])
-          ? (str = commaParser(factoryOutput[1])[1])
-          : str,
-        spaceParser(str) ? (str = spaceParser(str)[1]) : str)
-      : null
-
-    if (str === ']') return array
-  }
-  return [array, str.slice(1)]
-}
-const objectParser = str => {
-  if (str[0] !== '{') return null
-  // if (str.match(/\,(?!\s*[\{\"\w])/g)) {
-  //   throw SyntaxError('Invalid JSON')
-  // }
-  let object = {}
-  str = str.slice(1)
-  if (str.startsWith(' ') || str.startsWith('\n') || str.startsWith('\t')) {
-    spaceParser(str) ? (str = spaceParser(str)[1]) : str
-  }
-  while (str[0] != '}') {
-    str.startsWith(' ') && spaceParser(str) ? (str = spaceParser(str)[1]) : str
-    factoryOutput = stringParser(str)
-
-    if (factoryOutput) {
-      let key = factoryOutput[0]
-      if (factoryOutput[1]) {
-        str = factoryOutput[1]
-        str.startsWith(' ') && spaceParser(str)
-          ? (str = spaceParser(str)[1])
-          : str
-        colonParser(str) ? (str = colonParser(str)[1]) : str
-        str.startsWith(' ') && spaceParser(str)
-          ? (str = spaceParser(str)[1])
-          : str
-        let value = valueParser(str)
-        object[key] = value[0]
-        str = value[1]
-        commaParser(str) ? (str = commaParser(str)[1]) : str
-      }
-    }
-    if (str === '}') break
-  }
-  return [object, str.slice(1)]
-}
-
-const parsers = [
-  nullParser,
-  booleanParser,
-  numberParser,
-  stringParser,
-  arrayParser,
-  objectParser
-]
-
-const factoryParser = p => {
-  return function(text) {
+const factory = p => {
+  return text => {
     if (text === null) return null
     let out
-    for (let i = 0; i < p.length; i++) {
+    const keys = Object.keys(p)
+    for (let i = 0; i < keys.length; i++) {
       try {
-        out = p[i](text)
+        out = p[keys[i]](text)
       } catch (error) {
         console.log(error)
       }
@@ -154,7 +20,217 @@ const factoryParser = p => {
   }
 }
 
-let valueParser = factoryParser(parsers)
-let serialize = contents.replace(/\s(?=("[^"]*"|[^"])*$)/gm, '')
-parseJson(serialize)
-// console.log(stringParser('hello'))
+const colonRe = /^:/
+const commaRe = /^,/
+const numRe = /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/
+const commaErrRe = /\,(?!\s*[\{\"\w])/g
+const spaceRe = /^\s+|\s+$/
+const stringRe = /^"(?:\\"|.)*?"/
+const regexTable = {
+  '/\\\\/': '\\',
+  '/\\//': '/',
+  '/\\b/': '\b',
+  '/\\f/': '\f',
+  '/\\n/': '\n',
+  '/\\r/': '\r',
+  '/\\t/': '\t'
+}
+const validateRe = /[^ | \n]+$/
+const isValidNum = /^-?(0|[1-9]\d*|(?=\.))(\.\d+)?$/
+
+const space = str => {
+  let match
+  return (str && str.startsWith(' ')) || (str && str.startsWith('\n'))
+    ? ((match = str.match(spaceRe)),
+      match ? [match[0], str.replace(spaceRe, '')] : null)
+    : null
+}
+
+const nullx = str => {
+  space(str) ? (str = space(str)[1]) : str
+  return str && str.startsWith('null') ? [null, str.slice(4)] : null
+}
+
+const boolean = str => {
+  return str != undefined && str.startsWith('false')
+    ? [false, str.slice(5)]
+    : str && str.startsWith('true') ? [true, str.slice(4)] : null
+}
+
+const syntaxCheck1 = str => {
+  if (str.match(commaErrRe)) {
+    console.log(`\x1b[31m${'Message: Property expected'}\x1b[0m`)
+    throw SyntaxError('Invalid JSON')
+  }
+}
+
+const syntaxCheck2 = str => {
+  if (!str.startsWith('"'))
+    throw SyntaxError(`\x1b[31m${'Invalid JSON'}\x1b[0m`)
+}
+const syntaxCheck3 = str => {
+  if (!str.startsWith(':')) {
+    console.log(`\x1b[31m${'Message: Expected colon'}\x1b[0m`)
+    throw SyntaxError('Invalid JSON')
+  }
+  return str
+}
+
+const syntaxCheck4 = value => {
+  if (value === null) {
+    console.log(
+      '\x1b[31m%s\x1b[0m',
+      'Message: Must contain a value after colon'
+    )
+    throw SyntaxError('Invalid JSON')
+  }
+}
+const commaCheck = str => {
+  if (isFinite(str[0]) && str[1] === ' ') {
+    console.log('\x1b[31m%s\x1b[0m', 'Message: Expected comma')
+    throw SyntaxError('Invalid JSON')
+  }
+}
+const trailCheck = str => {
+  if (str[0] === ',') {
+    space(str.slice(1)) ? (str = space(str.slice(1))[1]) : str
+    if (str[0] === ']') {
+      console.log('\x1b[31m%s\x1b[0m', 'Message: Trailing comma')
+      throw SyntaxError('Invalid JSON')
+    }
+  }
+  return str
+}
+const numValidCheck = num => {
+  if (!isValidNum.test(num)) {
+    let message = `Message: '${num} 'is not a valid number.`
+    console.log(`\x1b[31m${message}\x1b[0m`)
+    throw SyntaxError('Invalid JSON')
+  }
+}
+const numSyntaxCheck = str => {
+  if (str.startsWith('.')) {
+    let message = `Message: Value expected before dot`
+    console.log(`\x1b[31m${message}\x1b[0m`)
+    throw SyntaxError('Invalid JSON')
+  }
+}
+
+const number = str => {
+  numSyntaxCheck(str)
+  space(str) ? (str = space(str)[1]) : str
+  let match = str.match(numRe)
+
+  if (match && match[0].includes('.')) {
+    numValidCheck(match[0])
+  }
+  if (match) {
+    return [parseFloat(match[0]), str.slice(match[0].length)]
+  }
+  return null
+}
+
+const stringx = str => {
+  let match
+  return str.startsWith('"')
+    ? ((match = str.match(stringRe)),
+      match && match[0] != undefined
+        ? [stringEnhancer(match[0]), str.replace(match[0], '')]
+        : SyntaxError('Syntax Error'))
+    : null
+}
+const stringEnhancer = str => {
+  str = str.slice(1, -1)
+  for (const key in regexTable) {
+    str = str.replace(new RegExp(key, 'g'), regexTable[key])
+  }
+  return str
+}
+
+const comma = str => {
+  let match
+  return str && str.startsWith(',')
+    ? ((match = str.match(commaRe)), match ? [match[0], str.slice(1)] : null)
+    : null
+}
+
+const array = str => {
+  space(str) ? (str = space(str)[1]) : str
+  if (str != undefined && !str.startsWith('[')) {
+    return null
+  }
+  let array = []
+  str = str.slice(1)
+  while (str[0] !== ']') {
+    space(str) ? (str = space(str)[1]) : str
+    commaCheck(str)
+    const factoryOut = valueParser(str)
+    if (factoryOut) {
+      array.push(factoryOut[0])
+      str = factoryOut[1]
+      str = trailCheck(str)
+      comma(factoryOut[1]) ? (str = comma(factoryOut[1])[1]) : str
+      space(str) ? (str = space(str)[1]) : str
+    }
+    if (!str.includes(']')) throw Error('Expected comma or closing brace')
+    if (str === ']') return [array, str.slice(1)]
+  }
+  return [array, str.slice(1)]
+}
+
+const colon = str => {
+  let match
+  return (match = str.match(colonRe)), match ? [match, str.slice(1)] : null
+}
+
+const object = str => {
+  space(str) ? (str = space(str)[1]) : str
+  if (str[0] !== '{') return null
+  syntaxCheck1(str)
+  let object = {}
+  str = str.slice(1)
+  space(str) ? (str = space(str)[1]) : str
+  while (str[0] != '}') {
+    space(str) ? (str = space(str)[1]) : str
+    syntaxCheck2(str)
+    let factory
+    try {
+      factory = stringx(str)
+    } catch (error) {}
+    space(str) ? (str = space(str)[1]) : str
+    factory[1] = syntaxCheck3(factory[1])
+    if (factory) {
+      let key = factory[0]
+      if (factory[1]) {
+        str = factory[1]
+        colon(str) ? (str = colon(str)[1]) : str
+        space(str) ? (str = space(str)[1]) : str
+        syntaxCheck4(valueParser(str))
+        let value = valueParser(str)
+        object[key] = value[0]
+        str = value[1]
+        comma(str) ? (str = comma(str)[1]) : str
+        space(str) ? (str = space(str)[1]) : str
+      }
+    }
+    if (str === '}') break
+  }
+  return [object, str.slice(1)]
+}
+
+const parsers = [nullx, boolean, number, stringx, array, object]
+const valueParser = factory(parsers)
+
+console.time()
+const result = valueParser(contents)
+console.timeEnd()
+if (result.length !== 2) {
+  console.log(result)
+}
+
+if (result) {
+  const test = validateRe.test(result[1])
+  test
+    ? console.log(`\x1b[31m${'Invalid JSON'}\x1b[0m`)
+    : console.log('\x1b[32m%s\x1b[0m', JSON.stringify(result[0], true, 2))
+}
